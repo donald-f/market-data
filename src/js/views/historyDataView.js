3146b1b09b6dbbc3a5a92489a0c0197e3a6e9d1a
donald-f <donald.flobeck@gmail.com>
2022-08-07 17:34:23 -0400

    Added Day Return colulumn

--- src/js/views/historyDataView.js
+++ src/js/views/historyDataView.js
@@ -10,24 +10,33 @@ class HistoryDataView extends View {
       <thead class="stick">
         <tr>
           <th>Date</th>
+          <th class="non-essential-cell">Volume</th>
           <th class="non-essential-cell">Open</th>
           <th class="non-essential-cell">High</th>
           <th class="non-essential-cell">Low</th>
           <th>Close</th>
-          <th class="non-essential-cell">Volume</th>
+          <th>Day Return</th>
         </tr>
       </thead>
       <tbody>
     `;
-    this._data.intervalData.forEach((element) => {
+    this._data.intervalData.forEach((element, i) => {
+      // get return over prior business day (if present)
+      const dayReturn = this._data.intervalData[i + 1]
+        ? (
+            (+element.close / +this._data.intervalData[i + 1].close - 1) *
+            100
+          ).toFixed(2) + '%'
+        : '';
       markup += `
     <tr>
       <td class="dates">${element.datetime}</td>
+      <td class="non-essential-cell">${element.volume}</td>
       <td class="non-essential-cell">${element.open}</td>
       <td class="non-essential-cell">${element.high}</td>
       <td class="non-essential-cell">${element.low}</td>
       <td>${element.close}</td>
-      <td class="non-essential-cell">${element.volume}</td>
+      <td>${dayReturn}</td>
     </tr>`;
     });
     markup += '</tbody></table>';
