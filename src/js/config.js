f7b52e158e68255de4148eb78c125b8e96bbd0ac
donald-f <donald.flobeck@gmail.com>
2023-02-12 18:04:09 -0500

    Refactored the model.js to make the fetch URLs and their parameters easier to understand. I will next work on including the first day return (if one is available)

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const TIMEOUT_SEC = 10;
 export const KEY = '9120747530c34355afd04d8a8b055f77';
-export const API_URL = 'https://api.twelvedata.com';
+export const API_BASE_URL = 'https://api.twelvedata.com';
 export const RES_PER_PAGE = 50;
 export const CSV_DELIMITER = ',';
--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
-import { AJAX, SUBTRACT_DAYS, SUBTRACT_DAYS_FORMAT_DATE } from './helpers.js';
-import { KEY, RES_PER_PAGE, API_URL, CSV_DELIMITER } from './config.js';
+import { AJAX, SUBTRACT_DAYS_FORMAT_DATE } from './helpers.js';
+import { KEY, RES_PER_PAGE, API_BASE_URL, CSV_DELIMITER } from './config.js';
 
 export const state = {
   search: {
@@ -29,27 +29,82 @@ export const getStockResults = async function (query) {
       )
         query.timePeriod[1] = SUBTRACT_DAYS_FORMAT_DATE(1);
     }
+
+    /* 2/12/23 replaced timePeriod with timePeriodStartDate and timePeriodEndDate (this is easier to read)
     const timePeriod =
       typeof query.timePeriod === 'number' // if the time period is just a number, we know that we are doing a trailing return as opposed to date range.
         ? `start_date=${SUBTRACT_DAYS_FORMAT_DATE(
             query.timePeriod
           )} 00:00:01&end_date=${SUBTRACT_DAYS_FORMAT_DATE(1)} 23:59:59`
         : `start_date=${query.timePeriod[0]} 00:00:01&end_date=${query.timePeriod[1]} 23:59:59`; // we do 1 day back because we only care about previous days' closes
+    */
 
-    const queryURL = `${API_URL}/time_series?symbol=${query.symbol}&interval=1day&${timePeriod}&apikey=${KEY}`;
-    // YYYY-MM-DD
-    console.log(queryURL);
+    let timePeriodStartDate, timePeriodEndDate;
 
-    const data = await AJAX(queryURL);
-    if (data.code === 400) {
-      resetStateSearch();
-      throw new Error('Invalid symbol! Please try again.');
+    if (typeof query.timePeriod === 'number') {
+      timePeriodStartDate = `${SUBTRACT_DAYS_FORMAT_DATE(
+        query.timePeriod
+      )} 00:00:01`;
+      timePeriodEndDate = `${SUBTRACT_DAYS_FORMAT_DATE(1)} 23:59:59`;
+    } else {
+      timePeriodStartDate = `${query.timePeriod[0]} 00:00:01`;
+      timePeriodEndDate = `${query.timePeriod[1]} 23:59:59`;
     }
-    state.search.csvURL = `${queryURL}&format=CSV&delimiter=${CSV_DELIMITER}`;
+
+    // const queryURL = `${API_URL}/time_series?symbol=${query.symbol}&interval=1day&${timePeriod}&apikey=${KEY}`; 2/12/23...replaced with the below cleaner code -- more lines but easier to read
+    const apiUrlConstructor = function (type, firstDayRetEndDate) {
+      // types will be undefined, firstDayReturn, or csv
+      const url = new URL(API_BASE_URL + '/time_series');
+      url.searchParams.set('apikey', KEY);
+      url.searchParams.set('symbol', query.symbol);
+      url.searchParams.set('interval', '1day');
+      if (type === 'firstDayReturn') {
+        url.searchParams.set('end_date', timePeriodStartDate);
+        url.searchParams.set('outputsize', 2);
+        return url;
+      }
+      url.searchParams.set('end_date', timePeriodEndDate);
+      url.searchParams.set('start_date', timePeriodStartDate);
+      if (type === 'csv') {
+        url.searchParams.set('format', 'CSV');
+        url.searchParams.set('delimiter', CSV_DELIMITER);
+      }
+      return url;
+    };
+    const queryURL = apiUrlConstructor();
+    const firstDayURL = apiUrlConstructor('firstDayReturn');
+
+    const [queryResult, firstDayQueryResult] = await Promise.allSettled([
+      AJAX(queryURL),
+      AJAX(firstDayURL),
+    ]);
+
+    if (
+      queryResult.status !== 'fulfilled' ||
+      firstDayQueryResult.status !== 'fulfilled'
+    ) {
+      throw new Error('Something went wrong. Please try again.');
+    }
+    const data = queryResult.value;
+    const firstDayRetData = firstDayQueryResult.value;
+    console.log(data);
+    console.log(firstDayRetData);
+    // WILL NEED TO THINK ABOUT THE BEST WAY TO GET THE FIRST DAY'S RETURN.
+
+    if (data.code === 400 || firstDayRetData.code === 400) {
+      resetStateSearch();
+      throw new Error(
+        'Invalid query. Either the symbol is unavailable or the date range is invalid. Please try again.'
+      );
+    }
+    // state.search.csvURL = `${queryURL}&format=CSV&delimiter=${CSV_DELIMITER}`; 2/12/23 replaced this with the three below rows
+
+    state.search.csvURL = apiUrlConstructor('csv');
+
     // https://api.twelvedata.com/stocks?symbol=AAPL&exchange=NASDAQ&apikey=9120747530c34355afd04d8a8b055f77
     state.search.page = 1;
     const possibleSecFullNames = await AJAX(
-      `${API_URL}/symbol_search?symbol=${data.meta.symbol}&outputsize=120`
+      `${API_BASE_URL}/symbol_search?symbol=${data.meta.symbol}&outputsize=120`
     );
     const secFullName =
       possibleSecFullNames.data.find(
